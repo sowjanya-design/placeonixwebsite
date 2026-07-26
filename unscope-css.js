f7a5eebf7abc7b33e18c627ed7612a6398e04afb
Junnuri Mohan Karthikeya <sowjanya@placeonix.com>
2026-07-26 16:31:02 +0530

    chore: Clean up unused legacy scripts and static assets

--- unscope-css.js
+++
@@ -1,7 +0,0 @@
-
-const fs = require('fs');
-let css = fs.readFileSync('web-next/src/app/home.css', 'utf8');
-css = css.replace(/#home-scope /g, '');
-fs.writeFileSync('web-next/src/app/home.css', css);
-console.log('Unscoped');
-
